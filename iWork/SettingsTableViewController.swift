2dc738f26cf713fca9f225ef3436a971f4de2863
Erick Sanchez <esericksanc@gmail.com>
2017-07-06 21:27:32 -0700

    CRUD Employers
    TODO: rename

--- iWork/SettingsTableViewController.swift
+++ iWork/SettingsTableViewController.swift
@@ -14,7 +14,7 @@ fileprivate enum CDSettingsHierarchy {
     case employers
 }
 
-class SettingsTableViewController: UITableViewController {
+class SettingsTableViewController: FetchedResultsTableViewController {
     
     fileprivate var hierarchy: CDSettingsHierarchy = .root
     
@@ -25,20 +25,36 @@ class SettingsTableViewController: UITableViewController {
     // MARK: Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         switch hierarchy {
         case .root, .employers:
             return 1
         }
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch hierarchy {
+        case .root:
+            return "Select an Employer"
+        default:
+            return nil
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         switch hierarchy {
         case .root:
             return 1
         case .employers:
-            return array.count
+            return fetchedResultsController.sections![section].numberOfObjects
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        switch hierarchy {
+        case .employers:
+            return (fetchedResultsController.object(at: indexPath).objectID != appDelegate.currentEmployer.objectID)
+        default:
+            return false
         }
     }
     
@@ -49,13 +65,13 @@ class SettingsTableViewController: UITableViewController {
         switch hierarchy {
         case .root:
             let cell = returnCell()
-            cell.textLabel!.text = "Selected Employer"
+            cell.textLabel!.text = appDelegate.currentEmployer.name
             cell.accessoryType = .disclosureIndicator
             
             return cell
         case .employers:
             let cell = returnCell(forIdentifier: "subtitle")
-            let employer = array[indexPath.row] as! Employer
+            let employer = fetchedResultsController.object(at: indexPath) as! Employer
             cell.textLabel!.text = String("\(employer.name!) - \(employer.selectedRole!.title!)")
             cell.detailTextLabel!.text = String("Number of Roles: \(employer.roles?.count ?? 0)")
             if employer.objectID == appDelegate.currentEmployer.objectID {
@@ -70,6 +86,22 @@ class SettingsTableViewController: UITableViewController {
     
     // MARK: - VOID METHODS
     
+    private var reloadIndexesOnViewDidAppear: [IndexPath]?
+    
+    private var fetchedResultsController: NSFetchedResultsController<NSManagedObject>! {
+        didSet {
+            if fetchedResultsController != nil {
+                do {
+                    try fetchedResultsController.performFetch()
+                    fetchedResultsController.delegate = self
+                    tableView.reloadData()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
     private func updateTable() {
         switch hierarchy {
         case .root:
@@ -77,7 +109,7 @@ class SettingsTableViewController: UITableViewController {
         case .employers:
             let fetch: NSFetchRequest<Employer> = Employer.fetchRequest()
             fetch.sortDescriptors = CTSortDescriptor(key: "name")
-            array = try! container.viewContext.fetch(fetch)
+            fetchedResultsController = NSFetchedResultsController<NSManagedObject>(fetchRequest: fetch as! NSFetchRequest<NSManagedObject>, managedObjectContext: container.viewContext, sectionNameKeyPath: nil, cacheName: nil)
         }
         
         tableView.reloadData()
@@ -101,10 +133,23 @@ class SettingsTableViewController: UITableViewController {
             let vc = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "settingsVC") as! SettingsTableViewController
             vc.hierarchy = .employers
             self.navigationController?.pushViewController(vc, animated: true)
+            
+            reloadIndexesOnViewDidAppear = [indexPath]
         case .employers:
-            let row = array[indexPath.row] as! Employer
+            let row = fetchedResultsController.object(at: indexPath) as! Employer
             appDelegate.currentEmployer = row
             self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            let row = fetchedResultsController.object(at: indexPath)
+            container.viewContext.delete(row)
+            appDelegate.saveContext()
+        default:
+            break
         }
     }
     
@@ -119,7 +164,6 @@ class SettingsTableViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { [weak self] (action) in
             _ = Employer(name: alert.inputField.text!, inContext: self!.container.viewContext)
             self!.appDelegate.saveContext()
-            self!.updateTable()
         }))
         self.present(alert, animated: true, completion: nil)
     }
@@ -140,5 +184,12 @@ class SettingsTableViewController: UITableViewController {
 
         // Uncomment the following line to preserve selection between presentations
         self.clearsSelectionOnViewWillAppear = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let indexesToReload = reloadIndexesOnViewDidAppear {
+            tableView.reloadRows(at: indexesToReload, with: .fade)
+            reloadIndexesOnViewDidAppear = nil
+        }
     }
 }
