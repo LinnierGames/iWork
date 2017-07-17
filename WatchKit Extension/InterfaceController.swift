a260e77ff48d11c52d41e612b9c28fc9422568e6
Erick Sanchez <esericksanc@gmail.com>
2017-07-16 20:30:47 -0700

    Segue to Punch Clock

--- WatchKit Extension/InterfaceController.swift
+++ WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,38 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var tableView: WKInterfaceTable!
+    
+    private struct Table {
+        static var rowTask = 0
+        static var rowPunchClock = 1
+    }
+    
+    private var rowTask: WKCustomTableRow! {
+        didSet {
+            rowTask.textLabel.setText("Task Manager")
+        }
+    }
+    private var rowPunchClock: WKCustomTableRow! {
+        didSet {
+            rowPunchClock.textLabel.setText("Punch Clock")
+        }
+    }
+    
+    // MARK: - RETURN VALUES
+    
+    // MARK: - VOID METHODS
+    
+    // MARK: - IBACTIONS
+    
+    // MARK: - LIFE CYCLE
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        rowTask = tableView.rowController(at: Table.rowTask) as! WKCustomTableRow
+        rowPunchClock = tableView.rowController(at: Table.rowPunchClock) as! WKCustomTableRow
     }
     
     override func willActivate() {
---
+++ WatchKit Extension/WKCustomTableRow.swift
@@ -0,0 +1,20 @@
+//
+//  WKCustomTableRow.swift
+//  iWork
+//
+//  Created by Erick Sanchez on 7/16/17.
+//  Copyright © 2017 Erick Sanchez. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class WKCustomTableRow: NSObject {
+
+    @IBOutlet var textLabel: WKInterfaceLabel!
+    @IBOutlet var punchPrefix: WKInterfaceLabel!
+    @IBOutlet var timeStamp: WKInterfaceLabel!
+    @IBOutlet var interval: WKInterfaceLabel!
+
+    
+}
