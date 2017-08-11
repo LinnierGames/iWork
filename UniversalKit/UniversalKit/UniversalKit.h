ab2c15bf98edcfc647f704ac838bf03230a0dc7a
Erick Sanchez <esericksanc@gmail.com>
2017-08-10 21:01:53 -0700

    Init UniversalKit

---
+++ UniversalKit/UniversalKit/UniversalKit.h
@@ -0,0 +1,17 @@
+//
+//  UniversalKit.h
+//  UniversalKit
+//
+//  Created by Erick Sanchez on 8/10/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UniversalKit.
+FOUNDATION_EXPORT double UniversalKitVersionNumber;
+
+//! Project version string for UniversalKit.
+FOUNDATION_EXPORT const unsigned char UniversalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UniversalKit/PublicHeader.h>
---
+++ UniversalKit/UniversalKit/UniversalKit.swift
@@ -0,0 +1,15 @@
+//
+//  UniversalKit.swift
+//  UniversalKit
+//
+//  Created by Erick Sanchez on 8/10/17.
+//
+//
+
+import Foundation
+
+public struct UniversalOperations {
+    public static let sharedInstance = UniversalOperations()
+    
+    public lazy var 
+}
